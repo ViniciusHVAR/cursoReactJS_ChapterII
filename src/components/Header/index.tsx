2a82e66429ab826a4df59dd7742ed997a2dc52f6
ViniciusHVAR <vinicius.martins@hvarconsulting.com.br>
2024-07-11 12:33:52 -0300

    feat: new transaction button and modal now working.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import logoImg from '../../assets/logo.svg'
-import { Container, Content} from './styles'
+import { Container, Content } from './styles'
 
-export function Header() {
+interface IHeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: IHeaderProps) {
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
             </Content>    
