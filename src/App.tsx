fb0b6f47b7fc7a579bcc0617f6866950665d7830
ViniciusHVAR <vinicius.martins@hvarconsulting.com.br>
2024-07-15 13:29:22 -0300

    feat: modal style implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
@@ -23,12 +24,10 @@ export function App() {
       
       <Dashboard />
       
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar Transação</h2>
-      </Modal>
+      />
 
       <GlobalStyle />
     </>
